68cf916c37e66feb922cf06c95c34eb5ef082d81
kingvision911 <kingvision911@gmail.com>
2026-04-02 11:37:54 +0300

    commecnt local port

--- server.js
+++ server.js
@@ -571,9 +571,9 @@ cron.schedule("0 0 * * *", () => {
     sendDailyReport();
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+// app.listen(3000, () => {
+//     console.log("Server running on port 3000");
+// });
 
 const PORT = process.env.PORT || 3000;
 
